f481ecc512b6f870accff8148ad23eb7feab6e68
Brian Matamet <patrickmat101@gmail.com>
2020-11-28 14:14:52 -0800

    Looped cacti

--- speakToDino.py
+++ speakToDino.py
@@ -49,11 +49,51 @@ backFgY = 0
 backFgVel = 0
 
 # Load cactus images
-c1 = pygame.transform.scale(pygame.image.load("Cacti/cactus1.png"), (150, 150))
-c2 = pygame.transform.scale(pygame.image.load("Cacti/cactus2.png"), (150, 150))
-c3 = pygame.transform.scale(pygame.image.load("Cacti/cactus3.png"), (150, 150))
-c4 = pygame.transform.scale(pygame.image.load("Cacti/cactus4.png"), (150, 150))
-c5 = pygame.transform.scale(pygame.image.load("Cacti/cactus5.png"), (150, 150))
+c1 = pygame.transform.scale(pygame.image.load("Cacti/cactus1.png").convert_alpha(), (115, 115))
+c2 = pygame.transform.scale(pygame.image.load("Cacti/cactus2.png").convert_alpha(), (115, 115))
+c3 = pygame.transform.scale(pygame.image.load("Cacti/cactus3.png").convert_alpha(), (115, 115))
+c4 = pygame.transform.scale(pygame.image.load("Cacti/cactus4.png").convert_alpha(), (115, 115))
+c5 = pygame.transform.scale(pygame.image.load("Cacti/cactus5.png").convert_alpha(), (115, 115))
+c6 = pygame.transform.scale(pygame.image.load("Cacti/cactus1.png").convert_alpha(), (115, 115))
+c7 = pygame.transform.scale(pygame.image.load("Cacti/cactus2.png").convert_alpha(), (115, 115))
+c8 = pygame.transform.scale(pygame.image.load("Cacti/cactus3.png").convert_alpha(), (115, 115))
+c9 = pygame.transform.scale(pygame.image.load("Cacti/cactus4.png").convert_alpha(), (115, 115))
+c10 = pygame.transform.scale(pygame.image.load("Cacti/cactus5.png").convert_alpha(), (115, 115))
+c11 = pygame.transform.scale(pygame.image.load("Cacti/cactus1.png").convert_alpha(), (115, 115))
+c12 = pygame.transform.scale(pygame.image.load("Cacti/cactus2.png").convert_alpha(), (115, 115))
+c13 = pygame.transform.scale(pygame.image.load("Cacti/cactus3.png").convert_alpha(), (115, 115))
+c14 = pygame.transform.scale(pygame.image.load("Cacti/cactus4.png").convert_alpha(), (115, 115))
+c15 = pygame.transform.scale(pygame.image.load("Cacti/cactus5.png").convert_alpha(), (115, 115))
+
+
+# Cacti positioning
+cy = 460
+# obstacle 1
+c1x = 600
+# obstacle 2
+c2x = 1000
+c3x = 1025
+# obstacle 3
+c4x = 1425
+c5x = 1400
+# obstacle 4
+c10x = 1800
+# obstacle 5
+c7x = 2200
+c9x = 2225
+# obstacle 6
+c6x = 2600
+# obstacle 7
+c8x = 3000
+# obstacle 8
+c13x = 3400
+c15x = 3425
+# obstacle 9
+c12x = 3800
+c14x = 3820
+
+
+cactusVel = 0
 
 
 # Tile and Icon
@@ -64,19 +104,21 @@ pygame.display.set_icon(icon)
 # Player
 playerImg = pygame.image.load("base_images/dinosaur.png")
 playerX = 10
-playerY = 350
+playerY = 415
 playerX_change = 0
 playerY_change = 0
 
 # PlayerAnimation
-d1 = pygame.transform.scale(pygame.image.load("Dino/dino1.png"), (250,250))
-d2 = pygame.transform.scale(pygame.image.load("Dino/dino2.png"), (250,250))
-d3 = pygame.transform.scale(pygame.image.load("Dino/dino3.png"), (250,250))
-d4 = pygame.transform.scale(pygame.image.load("Dino/dino4.png"), (250,250))
-d5 = pygame.transform.scale(pygame.image.load("Dino/dino5.png"), (250,250))
-d6 = pygame.transform.scale(pygame.image.load("Dino/dino6.png"), (250,250))
-d7 = pygame.transform.scale(pygame.image.load("Dino/dino7.png"), (250,250))
-d8 = pygame.transform.scale(pygame.image.load("Dino/dino8.png"), (250,250))
+dinoW = 175
+dinoH = 175
+d1 = pygame.transform.scale(pygame.image.load("Dino/dino1.png").convert_alpha(), (dinoW,dinoH))
+d2 = pygame.transform.scale(pygame.image.load("Dino/dino2.png").convert_alpha(), (dinoW,dinoH))
+d3 = pygame.transform.scale(pygame.image.load("Dino/dino3.png").convert_alpha(), (dinoW,dinoH))
+d4 = pygame.transform.scale(pygame.image.load("Dino/dino4.png").convert_alpha(), (dinoW,dinoH))
+d5 = pygame.transform.scale(pygame.image.load("Dino/dino5.png").convert_alpha(), (dinoW,dinoH))
+d6 = pygame.transform.scale(pygame.image.load("Dino/dino6.png").convert_alpha(), (dinoW,dinoH))
+d7 = pygame.transform.scale(pygame.image.load("Dino/dino7.png").convert_alpha(), (dinoW,dinoH))
+d8 = pygame.transform.scale(pygame.image.load("Dino/dino8.png").convert_alpha(), (dinoW,dinoH))
 dinoRun = [d1, d2, d3, d4, d5, d6, d7, d8]
 runCount = 0
 
@@ -114,7 +156,7 @@ def player(x, y):
 # Game loop
 running = True
 while running:
-    pygame.time.delay(30)
+    pygame.time.delay(25 )
     # RGB values for screen
     screen.fill((0, 0, 0))
     # Background image
@@ -124,6 +166,35 @@ while running:
         backLayerX = 0
     if backFgX == -1920:
         backFgX = 0
+    if c1x == -3240:
+        c1x = 600
+    if c2x == -2840:
+        c2x = 1000
+    if c3x == -2815:
+        c3x = 1025
+    if c4x == -2415:
+        c4x = 1425
+    if c5x == -2440:
+        c5x = 1400
+    if c10x == -2040:
+        c10x = 1800
+    if c7x == -1640:
+        c7x = 2200
+    if c9x == -1615:
+        c9x = 2225
+    if c6x == -1240:
+        c6x = 2600
+    if c8x == -840:
+        c8x = 3000
+    if c13x == -440:
+        c13x = 3400
+    if c15x == -415:
+        c15x = 3425
+    if c12x == -40:
+        c12x = 3800
+    if c14x == -20:
+        c14x = 3820
+
 
     # player_rect.x = playerX
     # player_rect.y = playerY
@@ -142,51 +213,39 @@ while running:
     if not isJump:
         if keys[pygame.K_SPACE]:
             isJump = True
-        # add animation code?
     else:
         if jumpCount >= -10:
-            playerY -= (jumpCount * abs(jumpCount)) * 0.35
+            playerY -= (jumpCount * abs(jumpCount)) * 0.5
             jumpCount -= 1
         else:
             jumpCount = 10
             isJump = False
 
+    # Velocity of Images
+    backVel = -4
+    backLayerVel = -12
+    cactusVel = -12
+    backFgVel = -16
 
-        # # KEYDOWN - "checks to see if any keyboard key is being pressed"
-        # if event.type == pygame.KEYDOWN:
-        #     if event.key == pygame.K_LEFT and player_rect.x > 0 and backX < 0:
-        #         playerX_change = -2
-        #         backVel = 0.5
-        #         backCloseVel = 2
-        #     if event.key == pygame.K_RIGHT:
-        #         playerX_change = 2
-        #         backVel = -0.5
-        #         backCloseVel = -2
-        #     if not isJump:
-        #         if event.key == pygame.K_SPACE:
-        #             isJump = True
-        # # KEYUP - "Checks to see if any key is released (stopped pressing)"
-        # if event.type == pygame.KEYUP:
-        #     if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-        #         playerX_change = 0
-        #         backVel = 0
-        #         backCloseVel = 0
-        #
-        #     if jumpCount >= -10:
-        #         playerY -= (jumpCount * abs(jumpCount)) * 0.5
-        #         jumpCount -= 1
-        #     else:
-        #         jumpCount = 10
-        #         isJump = False
 
-    backVel = -2
-    backLayerVel = -8
-    backFgVel = -10
-
-    # Incorporate player movement
+    # Incorporate player/background movement
     playerX += playerX_change
     backX += backVel
     backLayerX += backLayerVel
+    c1x += cactusVel
+    c2x += cactusVel
+    c3x += cactusVel
+    c4x += cactusVel
+    c5x += cactusVel
+    c6x += cactusVel
+    c7x += cactusVel
+    c8x += cactusVel
+    c9x += cactusVel
+    c10x += cactusVel
+    c12x += cactusVel
+    c13x += cactusVel
+    c14x += cactusVel
+    c15x += cactusVel
     backFgX += backFgVel
 
     # Show background to screen
@@ -200,6 +259,34 @@ while running:
     screen.blit(background1, (backX + 1920, backY))
     screen.blit(background4, (backLayerX, backLayerY))
     screen.blit(background4, (backLayerX + 1920, backLayerY))
+    screen.blit(c1, (c1x, cy))
+    screen.blit(c2, (c2x, cy))
+    screen.blit(c3, (c3x, cy))
+    screen.blit(c4, (c4x, cy))
+    screen.blit(c5, (c5x, cy))
+    screen.blit(c6, (c6x, cy))
+    screen.blit(c7, (c7x, cy))
+    screen.blit(c8, (c8x, cy))
+    screen.blit(c9, (c9x, cy))
+    screen.blit(c10, (c10x, cy))
+    screen.blit(c12, (c12x, cy))
+    screen.blit(c13, (c13x, cy))
+    screen.blit(c14, (c14x, cy))
+    screen.blit(c15, (c15x, cy))
+    screen.blit(c1, (c1x + 3840,cy))
+    screen.blit(c2, (c2x + 3840,cy))
+    screen.blit(c3, (c3x + 3840,cy))
+    screen.blit(c4, (c4x + 3840,cy))
+    screen.blit(c5, (c5x + 3840,cy))
+    screen.blit(c6, (c6x + 3840, cy))
+    screen.blit(c7, (c7x + 3840, cy))
+    screen.blit(c8, (c8x + 3840, cy))
+    screen.blit(c9, (c9x + 3840, cy))
+    screen.blit(c10, (c10x + 3840, cy))
+    screen.blit(c12, (c12x + 3840, cy))
+    screen.blit(c13, (c13x + 3840, cy))
+    screen.blit(c14, (c14x + 3840, cy))
+    screen.blit(c15, (c15x + 3840, cy))
 
     # Show player before foreground
     player(playerX, playerY)
@@ -209,4 +296,4 @@ while running:
     # Show score
     show_score(textX, textY)
     pygame.display.update()
-    # clock.tick(60)
+    # clock.tick(120)
